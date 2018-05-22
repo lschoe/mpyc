8505d73ae0ac2247fd603d868f8279db067bb15c
lschoe <l.a.m.schoenmakers@tue.nl>
2018-05-22 17:53:15 +0200

    Added secure sort demo.

--- demos/run-all.bat
+++ demos/run-all.bat
@@ -1,5 +1,6 @@
 call run %1 ot.py %2 %3 %4 %5 %6 %7 %8 %9
 call run %1 parallelsort.py %2 %3 %4 %5 %6 %7 %8 %9
+call run %1 sort.py %2 %3 %4 %5 %6 %7 %8 %9
 call run %1 indextounitvector.py %2 %3 %4 %5 %6 %7 %8 %9
 call run %1 secretsanta.py %2 %3 %4 %5 %6 %7 %8 %9
 call run %1 id3gini.py %2 %3 %4 %5 %6 %7 %8 %9
---
+++ demos/sort.py
@@ -0,0 +1,58 @@
+from mpyc.runtime import mpc
+
+def bsort(x):
+
+    def bitonic_sort(lo, n, up=True):
+        if n > 1:
+            m = n // 2
+            bitonic_sort(lo, m, not up)
+            bitonic_sort(lo + m, n - m, up)
+            bitonic_merge(lo, n, up)
+
+    def bitonic_merge(lo, n, up):
+        if n > 1:
+            # set m as the greatest power of 2 less than n:
+            m = 2**((n - 1).bit_length() - 1)
+            for i in range(lo, lo + n - m):
+                bitonic_compare(i, i + m, up)
+            bitonic_merge(lo, m, up)
+            bitonic_merge(lo + m, n - m, up)
+
+    def bitonic_compare(i, j, up):
+        up = secnum(bool(up))
+        b = (x[i] > x[j]) ^ ~up
+        d = b * (x[j] - x[i])
+        x[i], x[j] = x[i] + d, x[j] - d
+
+    bitonic_sort(0, len(x))
+    return x
+
+def main():
+    if mpc.args:
+        n = int(mpc.args[0])
+    else:
+        n = 5
+        print('Setting input to default =', n)
+
+    s = [(-1)**i * (i + (n//2))**2 for i in range(n)]
+
+    mpc.start()
+
+    global secnum
+
+    secnum = mpc.SecInt()
+    print('Using secure integers:', secnum)
+    x = list(map(secnum, s))
+    print('Array:', mpc.run(mpc.output(x)))
+    print('Sorted array:', mpc.run(mpc.output(bsort(x))))
+
+    secnum = mpc.SecFxp()
+    print('Using secure fixed-point numbers:', secnum)
+    x = list(map(secnum, s))
+    print('Input array:', mpc.run(mpc.output(x)))
+    print('Sorted array:', mpc.run(mpc.output(bsort(x))))
+
+    mpc.shutdown()
+
+if __name__ == '__main__':
+    main()
