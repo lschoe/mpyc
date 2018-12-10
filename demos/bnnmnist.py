48afc79c838bc44d302bacfd36ac822004ccf0df
lschoe <l.a.m.schoenmakers@tue.nl>
2018-12-10 16:35:26 +0100

    More complete benchmarking.

--- demos/bnnmnist.py
+++ demos/bnnmnist.py
@@ -16,24 +16,30 @@ The operations are performed using secure 14-bit integers. In the first
 layer and in the output layer, the secure comparisons are performed
 assuming the full 14-bit range for the input values. However, for the second
 and third layers, the input values are assumed to be limited to a 10-bit range,
-or more precisely, to the range [-493, 493]. The function bsgn() is used to 
-efficiently compare inputs from this range with 0.
+approximately. 
 
-Function bsgn(a) securely computes binary signs using the prime
-p = 13835556230699448671 as the modulus for the underlying prime field.
-Basically, bsgn(a) securely computes:
+Functions bsgn_1(a) and bsgn_2(a) securely compute binary signs using the primes
+p=9409569905028393239 and p=13835556230699448671, respectively, as the modulus 
+for the underlying prime field. Function bsgn_1(a) yields the correct result 
+for a in [-383, 383], and bsgn_2(a) is correct for a in [-493, 493].
+
+Basically, bsgn_1(a) securely computes:
+
+    (t | p), with t = sum((2(a+i)+1 | p) for i=-1,0,1),
+
+where (t | p) = t^((p-1)/2) mod p denotes the Legendre symbol.
+And, bsgn_2(a) securely computes:
 
     (t | p), with t = sum((2(a+i)+1 | p) for i=-2,-1,0,1,2),
 
-where (a | p) = a^((p-1)/2) mod p denotes the Legendre symbol.
-
 Benchmarking against the built-in MPyC integer comparison is also supported.
 The limited 10-bit range for the second and third layers is exploited in this
-case by setting the bit length for the comparison inputs to 10. 
+case by setting the bit length for the comparison inputs to 10.
 
-Additionally, functions vector_bsgn(x) and vector_sge(x), respectively, 
-compute bsgn(a) and 2*(a>=0)-1, for all elements a of x in parallel.
-This reduces the overall time spent on integer comparisons by a factor of 2 to 3.
+Additionally, functions vector_bsgn_1(x), vector_bsgn_2(x),and vector_sge(x),
+respectively, compute bsgn_1(a), bsgn_2(a), and 2*(a>=0)-1, for all elements a
+of x in parallel. This reduces the overall time spent on integer comparisons
+by a factor of 2 to 3.
 """
 
 import os
@@ -65,11 +71,11 @@ def load_b(name):
     return b
 
 @mpc.coroutine
-async def bsgn(a):
+async def bsgn_1(a):
     """Compute binary sign of a securely.
 
     Binary sign of a (1 if a>=0 else -1) is obtained by securely computing
-    (t | p), with t = sum((2(a+i)+1 | p) for i=-2,-1,0,1,2).
+    (u+v+w - u*v*w)/2 with u=(2a-1 | p), v=(2a+1 | p), and w=(2a+3 | p).
 
     Legendre symbols (a | p) for secret a are computed securely by evaluating
     (a s r^2 | p) in the clear for secret random sign s and secret random r modulo p,
@@ -81,11 +87,65 @@ async def bsgn(a):
     p = Zp.modulus
     legendre_p = lambda a: gmpy2.legendre(a.value, p)
 
+    s = mpc.random_bits(Zp, 3, signed=True) # 3 random signs
+    r = mpc._randoms(Zp, 3)
+    r = mpc.schur_prod(r, r) # 3 random squares modulo p
+    a, s, r = await mpc.gather(a, s, r)
+    y = [b + 2 * i for b in (2 * a + 1,) for i in (-1, 0, 1)]
+    y.append(s[0])
+    s.append(s[1])
+    r.append(s[2])
+    y = await mpc.schur_prod(y, s)
+    y = await mpc.output(await mpc.schur_prod(y, r))
+    h = [legendre_p(y[i]) for i in range(3)]
+    u, v, w = [s[i] * h[i] for i in range(3)]
+    uvw = h[0] * h[1] * h[2] * y[3]
+    return (u + v + w - uvw) / 2
+
+@mpc.coroutine
+async def vector_bsgn_1(x):
+    """Compute bsgn_1(a) for all elements a of x in parallel."""
+    stype = type(x[0])
+    n = len(x)
+    await mpc.returnType(stype, n)
+    Zp = stype.field
+    p = Zp.modulus
+    legendre_p = lambda a: gmpy2.legendre(a.value, p)
+
+    s = mpc.random_bits(Zp, 3*n, signed=True) # 3n random signs
+    r = mpc._randoms(Zp, 3*n)
+    r = mpc.schur_prod(r, r) # 3n random squares modulo p
+    x, s, r = await mpc.gather(x, s, r)
+    y = [b + 2 * i for b in (2 * a + 1 for a in x) for i in (-1, 0, 1)]
+    y.extend(s[:n])
+    s.extend(s[n:2*n])
+    r.extend(s[-n:])
+    y = await mpc.schur_prod(y, s)
+    y = await mpc.output(await mpc.schur_prod(y, r))
+    h = [legendre_p(y[j]) for j in range(3*n)]
+    t = [s[j] * h[j] for j in range(3*n)]
+    z = [h[3*j] * h[3*j+1] * h[3*j+2] * y[3*n + j] for j in range(n)]
+    q = (p + 1) >> 1 # q = 1/2 mod p
+    return [Zp((u.value + v.value + w.value - uvw.value)*q) for u, v, w, uvw in zip(*[iter(t)] * 3, z)]
+
+@mpc.coroutine
+async def bsgn_2(a):
+    """Compute binary sign of a securely.
+
+    Binary sign of a (1 if a>=0 else -1) is obtained by securely computing
+    (t | p), with t = sum((2a+2i+1 | p) for i=-2,-1,0,1,2).
+    """
+    stype = type(a)
+    await mpc.returnType(stype)
+    Zp = stype.field
+    p = Zp.modulus
+    legendre_p = lambda a: gmpy2.legendre(a.value, p)
+
     s = mpc.random_bits(Zp, 6, signed=True) # 6 random signs
     r = mpc._randoms(Zp, 6)
     r = mpc.schur_prod(r, r) # 6 random squares modulo p
     a, s, r = await mpc.gather(a, s, r)
-    y = [b + i for b in (a * 2 + 1,) for i in (-4, -2, 0, 2, 4)]
+    y = [b + 2 * i for b in (2 * a + 1,) for i in (-2, -1, 0, 1, 2)]
     y = await mpc.schur_prod(y, s[:-1])
     y.append(s[-1])
     y = await mpc.output(await mpc.schur_prod(y, r))
@@ -94,8 +154,8 @@ async def bsgn(a):
     return s[-1] * legendre_p(t)
 
 @mpc.coroutine
-async def vector_bsgn(x):
-    """Compute binary signs securely for all elements of x in parallel."""
+async def vector_bsgn_2(x):
+    """Compute bsgn_2(a) for all elements a of x in parallel."""
     stype = type(x[0])
     n = len(x)
     await mpc.returnType(stype, n)
@@ -107,7 +167,7 @@ async def vector_bsgn(x):
     r = mpc._randoms(Zp, 6*n)
     r = mpc.schur_prod(r, r) # 6n random squares modulo p
     x, s, r = await mpc.gather(x, s, r)
-    y = [b + i for b in (a * 2 + 1 for a in x) for i in (-4, -2, 0, 2, 4)]
+    y = [b + 2 * i for b in (2 * a + 1 for a in x) for i in (-2, -1, 0, 1, 2)]
     y = await mpc.schur_prod(y, s[:-n])
     y.extend(s[-n:])
     y = await mpc.output(await mpc.schur_prod(y, r))
@@ -121,7 +181,7 @@ async def vector_sge(x):
 
     Vectorized version of MPyC's built-in secure comparison.
     Cf. mpc.sgn() with GE=True (and EQ=False).
-    
+
     NB: mpc.prod() and mpc.is_zero_public() are not (yet) vectorized.
     """
     stype = type(x[0])
@@ -173,22 +233,32 @@ async def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--batch-size', type=int, metavar='B',
-                       help='number of images to classify')
+                        help='number of images to classify')
     parser.add_argument('-o', '--offset', type=int, metavar='O',
-                       help='offset for batch (otherwise random in [0,10000-B])')
+                        help='offset for batch (otherwise random in [0,10000-B])')
+    parser.add_argument('-d', '--d-k-star', type=int, metavar='D',
+                        help='k=D=1,2 for Legendre-based comparison using d_k^*')
     parser.add_argument('--no-legendre', action='store_true',
-                       default=False, help='disable Legendre-based comparison')
+                        default=False, help='disable Legendre-based comparison')
     parser.add_argument('--no-vectorization', action='store_true',
-                       default=False, help='disable vectorization of comparisons')
-    parser.set_defaults(batch_size=1, offset=-1)
+                        default=False, help='disable vectorization of comparisons')
+    parser.set_defaults(batch_size=1, offset=-1, d_k_star=1)
     args = parser.parse_args()
-    
+
     batch_size = args.batch_size
     offset = args.offset
     if args.no_legendre:
         secnum = mpc.SecInt(14) # using vectorized MPyC integer comparison
     else:
-        secnum = mpc.SecInt(14, p=13835556230699448671) # Legendre-2 range [-493, 493]
+        if args.d_k_star == 1:
+            secnum = mpc.SecInt(14, p=9409569905028393239) # Legendre-1 range [-383, 383]
+            bsgn = bsgn_1
+            vector_bsgn = vector_bsgn_1
+        else:
+            secnum = mpc.SecInt(14, p=13835556230699448671) # Legendre-2 range [-493, 493]
+            bsgn = bsgn_2
+            vector_bsgn = vector_bsgn_2
+
     one_by_one = args.no_vectorization
 
     await mpc.start()
