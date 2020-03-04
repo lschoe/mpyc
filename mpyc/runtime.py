7bd0fdc27f4a955bade927cb7dcf39ec49f9c0c9
Bart Kamphorst <bart.kamphorst@tno.nl>
2020-03-04 17:49:17 +0100

    Fixed conversion of signed secrets.

--- mpyc/runtime.py
+++ mpyc/runtime.py
@@ -488,6 +488,9 @@ class Runtime:
         if issubclass(stype, sectypes.SecureFiniteField):
             for i in range(len(x)):
                 x[i] = self._mod(ttype(x[i]), stype.field.modulus)
+                if stype.field.is_signed and ttype.field.is_signed:
+                    is_neg = x[i] > stype.field.modulus >> 1
+                    x[i] = x[i] + is_neg * (ttype.field.modulus - stype.field.modulus)
         if d > 0:
             for i in range(len(x)):
                 x[i] <<= d
