899fa2c4a171d37e039c74d3e55f20a7a1abf15d
Bart Kamphorst <bart.kamphorst@tno.nl>
2020-06-10 12:16:41 +0200

    Fix incorrect handling of conversion of secfxp for different number of fractional bits.

--- mpyc/runtime.py
+++ mpyc/runtime.py
@@ -533,7 +533,7 @@ class Runtime:
                 if stype.field.is_signed and ttype.field.is_signed:
                     is_neg = x[i] > stype.field.modulus >> 1
                     x[i] = x[i] + is_neg * (ttype.field.modulus - stype.field.modulus)
-        if d > 0:
+        elif d > 0:
             for i in range(n):
                 x[i] <<= d
         return x
