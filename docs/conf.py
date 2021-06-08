aa14c33c1d2bd6859bf32fb0623f6e6cf8f1ecba
Berry Schoenmakers <l.a.m.schoenmakers@tue.nl>
2021-06-08 18:06:24 +0200

    Enable MPyC logo for Read the Docs.

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,6 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
 #html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
@@ -92,7 +91,7 @@ html_static_path = []
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
-
+html_logo = '../images/MPyC_Logo.png'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -127,3 +126,4 @@ latex_documents = [
      'Berry Schoenmakers', 'manual', True),
 ]
 #latex_use_modindex = False
+latex_logo = '../images/MPyC_Logo.png'
