1636d55387bc6f4e0ed74e5fbfc79275139380e6
Berry Schoenmakers <l.a.m.schoenmakers@tue.nl>
2023-09-04 11:17:11 +0200

    Demote use of mpc.run(), easier with PyScript.

--- demos/helloworld.py
+++ demos/helloworld.py
@@ -37,6 +37,11 @@ This way there is no need to lower the threshold t.
 
 from mpyc.runtime import mpc
 
-mpc.run(mpc.start())     # connect to all other parties
-print(''.join(mpc.run(mpc.transfer('Hello world!'))))
-mpc.run(mpc.shutdown())  # disconnect, but only once all other parties reached this point
+
+async def main():
+    await mpc.start()     # connect to all other parties
+    print(''.join(await mpc.transfer('Hello world!')))
+    await mpc.shutdown()  # disconnect, but only once all other parties reached this point
+
+if __name__ == '__main__':
+    mpc.run(main())
